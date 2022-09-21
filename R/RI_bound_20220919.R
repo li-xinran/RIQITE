687e40c4813c85b059eee9e7ad5da9f32ec96167
li-xinran <57820885+li-xinran@users.noreply.github.com>
2022-09-20 23:34:33 -0500

    add bound null and power simulation

---
+++ R/RI_bound_20220919.R
@@ -0,0 +1,189 @@
+
+test_stat <- function(z, y, method.list = list(name = "Wilcoxon")){
+  if(method.list$name == "DIM"){
+    stat = mean(y[z==1]) - mean(y[z==0])
+    return(stat)
+  }
+  
+  if(method.list$name == "Wilcoxon"){
+    score = rank_score( n, method.list = method.list )
+    stat = sum( score[rank(y, ties.method = "first")[z==1]] )
+    return(stat)
+  }
+  
+  if(method.list$name == "Stephenson"){
+    n = length(y)
+    score = rank_score( n, method.list = method.list )
+    stat = sum( score[rank(y, ties.method = "first")[z==1]] ) 
+    return(stat)
+  }
+}
+
+pval_imp_both <- function(Z, Y, c, method.list, Z.perm = NULL, iter.max = 10^5){
+  Y1.imp = Y + (1-Z) * c
+  Y0.imp = Y - Z * c
+  m =  sum(Z)
+  
+  if( is.null(Z.perm) ){
+    Z.perm = matrix(0, nrow = n, ncol = iter.max)
+    for(iter in 1:iter.max){
+      Z.perm[sample(n, m), iter] = 1
+    }
+  }
+  
+  iter.max = ncol(Z.perm)
+  stat.perm = rep(NA, iter.max)
+  for(iter in 1:iter.max){
+    Z.iter = Z.perm[, iter] 
+    Y.imp = Z.iter * Y1.imp + (1-Z.iter) * Y0.imp
+    stat.perm[iter] = test_stat(z = Z.iter, y = Y.imp, method.list)
+  }
+  
+  stat.obs = test_stat(z = Z, y = Y, method.list)
+  
+  pval <- mean( stat.perm >= stat.obs )
+  return(pval)
+}
+
+
+pval_imp_control <- function(Z, Y, c, method.list, Z.perm = NULL, iter.max = 10^5){
+  Y1.imp = Y + (1-Z) * c
+  Y0.imp = Y - Z * c
+  m =  sum(Z)
+  
+  if( is.null(Z.perm) ){
+    Z.perm = matrix(0, nrow = n, ncol = iter.max)
+    for(iter in 1:iter.max){
+      Z.perm[sample(n, m), iter] = 1
+    }
+  }
+  
+  iter.max = ncol(Z.perm)
+  stat.perm = rep(NA, iter.max)
+  for(iter in 1:iter.max){
+    Z.iter = Z.perm[, iter] 
+    stat.perm[iter] = test_stat(z = Z.iter, y = Y0.imp, method.list)
+  }
+  
+  stat.obs = test_stat(z = Z, y = Y0.imp, method.list)
+  
+  pval <- mean( stat.perm >= stat.obs )
+  return(pval)
+}
+
+
+pval_max <- function(Z, Y, c, method.list, Z.perm = NULL, iter.max = 10^5, impute = "both"){
+  
+  if(impute == "both"){
+    pval = pval_imp_both(Z = Z, Y = Y, c = c, method.list = method.list, Z.perm = Z.perm, iter.max = iter.max)
+    return(pval)
+  }
+  
+  if(impute == "control"){
+    pval = pval_imp_control(Z = Z, Y = Y, c = c, method.list = method.list, Z.perm = Z.perm, iter.max = iter.max)
+    return(pval)
+  }
+}
+
+
+
+
+
+
+ci_max_effect <- function(Z, Y, method.list, Z.perm = NULL, iter.max = 10^5, impute = "both", alpha = 0.05, tol = 10^(-3) ){
+  
+  if( is.null(Z.perm) ){
+    Z.perm = matrix(0, nrow = n, ncol = iter.max)
+    for(iter in 1:iter.max){
+      Z.perm[sample(n, m), iter] = 1
+    }
+  }
+  
+  f <- function(c){
+    pval = pval_max(Z = Z, Y = Y, c = c, method.list = method.list, Z.perm = Z.perm, impute = impute)
+    return(pval - alpha)
+  }
+  
+  c_sol <- uniroot(f, interval = c(-max(data$Y) + min(data$Y), max(data$Y) - min(data$Y)), extendInt = "upX", tol = tol)$root
+  c_sol <- round(c_sol, digits = -log10(tol))
+  if(f(c_sol) > 0){
+    while( f(c_sol) > 0 ){
+      c_sol = c_sol - tol
+    }
+    c_sol = c_sol + tol
+  }else{
+    while( f(c_sol) <= 0 ){
+      c_sol = c_sol + tol
+    }
+  }
+  return(c_sol)
+}
+
+#' Randomization test for bounded null hypotheses
+#'
+#' Obtain the p-value for testing the bounded null hypothesis H0: tau <= c or H0: tau >= c
+#'
+#' @param Z An \eqn{n} dimensional treatment assignment vector.
+#' @param Y An \eqn{n} dimensional observed outcome vector.
+#' @param c A scalar or vector specifying the bounded null hypothesis.
+#' @param alternative A character takes value "greater" and "less" indicating the direction of the alternative hypothesis.
+#' @param method.list A list specifies the choice of the rank sum test
+#'   statistic. For example, list(name = "DIM") means the difference-in-means statistic, 
+#'   list(name="Wilcoxon") means the Wilcoxon rank sum statistic, 
+#'   and list(name = "Stephenson", s = 10) means the Stephenson rank sum statistic with parameter s=10.
+#' @param Z.perm A \eqn{n \times nperm} matrix that specifies the permutated assignments for approximating the null distribution of the test statistic.
+#' @param nperm A positive integer representing the number of permutations for
+#'   approximating the randomization distribution of the test statistic
+#' @param impute A character specifies the form of the test statistic. "control" means we use
+#' the imputed control potential outcomes in the test statistic, and "both" means we use the 
+#' imputed observed outcome in the test statistic. 
+#' @return The p-value for testing the specified null hypothesis of interest.
+#' @export
+pval_bound <- function(Z, Y, c=0, method.list = list(name = "DIM"), alternative = "greater", Z.perm = NULL, nperm = 10^5, impute = "control"){
+  if(alternative == "greater"){
+    pval = pval_max(Z=Z, Y=Y, c=c, method.list=method.list, Z.perm = Z.perm, iter.max = nperm, impute = impute)
+    return(pval)
+  }
+  if(alternative == "less"){
+    pval_max(Z=Z, Y=-1*Y, c=-1*c, method.list=method.list, Z.perm = Z.perm, iter.max = nperm, impute = impute)
+    return(pval)
+  }
+}
+
+#' Confidence interval for the maximum or minimum individual effect
+#'
+#' Obtain confidence interval for the maximum or minimum individual effect
+#'
+#' @param Z An \eqn{n} dimensional treatment assignment vector.
+#' @param Y An \eqn{n} dimensional observed outcome vector.
+#' @param alternative A character takes value "greater" and "less" indicating the direction of the alternative hypothesis.
+#' @param method.list A list specifies the choice of the rank sum test
+#'   statistic. For example, list(name = "DIM") means the difference-in-means statistic, 
+#'   list(name="Wilcoxon") means the Wilcoxon rank sum statistic, 
+#'   and list(name = "Stephenson", s = 10) means the Stephenson rank sum statistic with parameter s=10.
+#' @param Z.perm A \eqn{n \times nperm} matrix that specifies the permutated assignments for approximating the null distribution of the test statistic.
+#' @param nperm A positive integer representing the number of permutations for
+#'   approximating the randomization distribution of the test statistic.
+#' @param impute A character specifies the form of the test statistic. "control" means we use
+#' the imputed control potential outcomes in the test statistic, and "both" means we use the 
+#' imputed observed outcome in the test statistic. 
+#' @param alpha A numeric object specifies the level of the confidence interval. In particular, the confidence level is \eqn{1-\alpha}. 
+#' @param tol A numerical object specifying the precision of the obtained
+#'   confidence intervals. For example, if tol = 10^(-3), then the confidence
+#'   limits are precise up to 3 digits.
+#' @return The p-value for testing the specified null hypothesis of interest.
+#' @export
+ci_bound <- function(Z, Y, alternative = "greater", method.list = list(name = "DIM"),  Z.perm = NULL, nperm = 10^3, impute = "control", alpha = 0.05, tol = 10^(-3) ){
+  if(alternative == "greater"){
+    ci.bound = ci_max_effect(Z=Z, Y=Y, method.list=method.list, Z.perm = Z.perm, iter.max = nperm, impute = impute, alpha = alpha, tol = tol )
+    return(ci.bound)
+  }
+  if(alternative == "less"){
+    ci.bound = -1 * ci_max_effect(Z=Z, Y=-1*Y, method.list=method.list, Z.perm = Z.perm, iter.max = nperm, impute = impute, alpha = alpha, tol = tol )
+    return(ci.bound)
+  }
+}
+
+
+
+
---
+++ R/power_simu_20220919.R
@@ -0,0 +1,221 @@
+
+normal_gen_PO <- function(n){
+  mu0 = 0; sd0 = 1
+  mu1 = 1; sd1 = 1
+  
+  Y1 = rnorm(n, mean = mu1, sd = sd1)
+  Y0 = rnorm(n, mean = mu0, sd = sd0)
+  
+  return(data.frame(Y1=Y1, Y0=Y0))
+}
+
+
+#' Simulation for various Stephenson rank statistics
+#'
+#' Conduct simulation for inferring quantiles of individual treatment effects using various
+#' Stephenson rank statistics.
+#'
+#' @param gen A function that generates the potential outcomes.
+#' @param n A numerical object specifying the sample size.
+#' @param treat.prop A numerical object specifying the proportion of treated units. 
+#' @param iter.max A numerical object specifying the number of simulated data sets.
+#' @param Steph.s.vec A vector specifying the parameters for a sequence of Stephenson rank statistics under investigation.
+#' @param alternative A character takes value "greater" and "less", indicating the 
+#' direction of alternative hypotheses.
+#' @param alpha A numerical object specifying the significance level.
+#' @param nperm A positive integer representing the number of permutations for
+#'   approximating the randomization distribution of the rank sum statistic.
+#' @param tol A numerical object specifying the precision of the obtained
+#'   confidence intervals. For example, if tol = 10^(-3), then the confidence
+#'   limits are precise up to 3 digits.
+#' @param switch A logical object indicating whether performing treatment label
+#'   switching to make the treated group have larger size.
+#' @param print.iter An integer determining how frequently we report the progress of the simulation.
+#' @return A list with the elements 
+#'   \item{Steph.s.vec}{An vector specifying the parameters for a sequence of Stephenson rank statistics under investigation.}
+#'   \item{dat.all}{A list containing all simulated data sets.}
+#'   \item{Z.CRE}{A \eqn{n} by iter.max matrix containing all simulated assignments.}
+#'   \item{method.list.all}{A list describing the Stephenson rank statistics under investigation.}
+#'   \item{ci.upper.all}{A list containing the inferred upper confidence limits for all quantiles of individual effects for all simulated data sets using the specified Stephenson rank statistics.}
+#'   \item{ci.lower.all}{A list containing the inferred lower confidence limits for all quantiles of individual effects for all simulated data sets using the specified Stephenson rank statistics.}
+#'
+#' @export
+simu_power <- function(gen = normal_gen_PO, n = 100, treat.prop = 0.5, iter.max = 100,
+                        Steph.s.vec = c(2, 5, 10), 
+                        alternative = "greater", alpha = 0.05,
+                        nperm = 10^3, tol = 10^(-3), switch = TRUE, print.iter = 10
+                        ){
+  
+  m = floor( n * treat.prop )
+  Z.perm = assign_CRE(n, m, nperm)
+  stat.null.all = list()
+  method.list.all = list()
+  for( s.ind in 1:length(Steph.s.vec) ){
+    method.list.all[[s.ind]] = list( name = "Stephenson", s = Steph.s.vec[s.ind] )
+    stat.null.all[[s.ind]] = null_dist(n, m, method.list = method.list.all[[s.ind]], Z.perm = Z.perm )
+  }
+  print("Monte Carlo approximation for null distributions completed...")
+  
+  ci.upper.all = list()
+  ci.lower.all = list()
+  for( s.ind in 1:length(Steph.s.vec) ){
+    ci.upper.all[[s.ind]] = matrix(NA, nrow = n, ncol = iter.max)
+    ci.lower.all[[s.ind]] = matrix(NA, nrow = n, ncol = iter.max)
+  }
+  Z.CRE = assign_CRE(n, m, iter.max)
+  dat.all = list()
+  for(iter in 1:iter.max){
+    dat = gen(n)
+    dat.all[[iter]] = dat
+    
+    Z = Z.CRE[, iter]
+    Y = Z * dat$Y1 + (1-Z) * dat$Y0
+    
+    # pval_max(Z = Z, Y = Y, c = 0, method.list = method.list.all[[s.ind]], Z.perm = Z.perm, impute = "control")
+    
+    for( s.ind in 1:length(Steph.s.vec) ){
+      ci.iter.s = ci_quantile(Z = Z, Y = Y, 
+                            alternative = alternative, method.list = method.list.all[[s.ind]],
+                            stat.null = stat.null.all[[s.ind]], alpha = alpha, 
+                            tol = tol, switch = switch)
+      ci.upper.all[[s.ind]][, iter] = ci.iter.s$upper
+      ci.lower.all[[s.ind]][, iter] = ci.iter.s$lower
+    }
+
+    if( iter %% print.iter == 0 ){
+      print( paste0( "Simulation for ", iter, "th iteration completed..." ) )
+    }
+  }
+  
+  # for( s.ind in 1:length(Steph.s.vec) ){
+  #   print( mean( colSums(ci.lower.all[[s.ind]] > 0) ) )
+  # }
+  
+  return(list( 
+    Steph.s.vec = Steph.s.vec, 
+    dat.all = dat.all, 
+    Z.CRE = Z.CRE,
+    Z.perm = Z.perm, 
+    method.list.all = method.list.all, 
+    ci.upper.all = ci.upper.all, 
+    ci.lower.all = ci.lower.all
+    ))
+}
+
+#' Comparing the performance of various Stephenson rank statistics
+#'
+#' Summarize the simulation to compare the performance of various Stephenson rank statistics.
+#'
+#' @param result A list containing the result from the simu_power function.
+#' @param measure A character specifying how we compare different Stephenson rank statistics.
+#' If it equals "CI_n(c)", then we compare the average confidence limits for the number of units with effects passing the threshold cutoff. 
+#' If it equals "CI_tau(k)", then we compare the average confidence limits for the individual treatment effect at rank k. 
+#' If it equals "test_tau(k)", then we compare the power for testing whether the individual treatment effect at rank k passes the threshold cutoff. 
+#' @param alternative A character takes value "greater" and "less", indicating the 
+#' direction of alternative hypotheses.
+#' @param cutoff A numerical object specifying the cutoff. 
+#' @param k An integer that specifies the quantile of individual effects under investigation.
+#'
+#' @export
+summary_power <- function(result, measure = "CI_n(c)", alternative = "greater", cutoff = 0, k = NULL){
+  if(measure == "CI_n(c)" & alternative == "greater"){
+    tab = data.frame(s = result$Steph.s.vec, ci.mean = rep(NA, length(result$Steph.s.vec)))
+    for(i in 1:nrow(tab)){
+      tab$ci.mean[i] = mean( colSums( result$ci.lower.all[[i]] > cutoff ) )
+    }
+    return(tab)
+  }
+  
+  if(measure == "CI_n(c)" & alternative == "less"){
+    tab = data.frame(s = result$Steph.s.vec, ci.mean = rep(NA, length(result$Steph.s.vec)))
+    for(i in 1:nrow(tab)){
+      tab$ci.mean[i] = mean( colSums( result$ci.upper.all[[i]] < cutoff ) )
+    }
+    return(tab)
+  }
+  
+  
+  if(measure == "test_tau(k)" & alternative == "greater"){
+    tab = data.frame(s = result$Steph.s.vec, power.mean = rep(NA, length(result$Steph.s.vec)))
+    for(i in 1:nrow(tab)){
+      tab$power.mean[i] = mean( result$ci.lower.all[[i]][k, ] > cutoff )
+    }
+    return(tab)
+  }
+  
+  if(measure == "test_tau(k)" & alternative == "less"){
+    tab = data.frame(s = result$Steph.s.vec, power.mean = rep(NA, length(result$Steph.s.vec)))
+    for(i in 1:nrow(tab)){
+      tab$power.mean[i] = mean( result$ci.upper.all[[i]][k, ] < cutoff )
+    }
+    return(tab)
+  }
+  
+  if(measure == "CI_tau(k)" & alternative == "greater"){
+    tab = data.frame(s = result$Steph.s.vec, ci.mean = rep(NA, length(result$Steph.s.vec)))
+    for(i in 1:nrow(tab)){
+      tab$ci.mean[i] = median( result$ci.lower.all[[i]][k, ] )
+    }
+    return(tab)
+  }
+  
+  if(measure == "CI_tau(k)" & alternative == "less"){
+    tab = data.frame(s = result$Steph.s.vec, ci.mean = rep(NA, length(result$Steph.s.vec)))
+    for(i in 1:nrow(tab)){
+      tab$ci.mean[i] = median( result$ci.upper.all[[i]][k, ] )
+    }
+    return(tab)
+  }
+}
+
+
+if(FALSE){
+  
+  normal_gen_PO <- function(n){
+    mu0 = 0; sd0 = 1
+    mu1 = 1; sd1 = 0.5
+    
+    Y1 = rnorm(n, mean = mu1, sd = sd1)
+    Y0 = rnorm(n, mean = mu0, sd = sd0)
+    
+    return(data.frame(Y1=Y1, Y0=Y0))
+  }
+  
+  result = simu_power( gen = normal_gen_PO, n = 120, treat.prop = 0.5, iter.max = 100,
+                       Steph.s.vec = c(2, 4, 6, 10, 30, 60), 
+                       alternative = "greater", alpha = 0.05,
+                       nperm = 10^5, tol = 10^(-3), switch = TRUE )
+  
+  perform = summary_power(result, measure = "CI_n(c)", alternative = "greater", cutoff = 0)
+  
+  perform
+  
+  
+  library(utils)
+  
+  # para.table = rbind( expand.grid( c(-1, 0, 1, 2), 1, c(0:9)/2 ),
+  #        expand.grid( c(-1, 0, 1, 2), c(0:9)/2, 1 ) )
+  
+  sigma1.grid = c( c(1:4)/5, 1, c(3:9)/2 )
+  para.table = expand.grid( c(-1, 0, 1, 2), 1, sigma1.grid )
+  colnames(para.table) = c("tau0", "sigma0", "sigma1")
+  para.table
+  
+  jobid = 1
+  
+  normal_gen_PO <- function(n){
+    mu0 = 0; sd0 = para.table$sigma0[jobid]
+    mu1 = para.table$tau0[jobid]; sd1 = para.table$sigma1[jobid]
+    
+    Y1 = rnorm(n, mean = mu1, sd = sd1)
+    Y0 = rnorm(n, mean = mu0, sd = sd0)
+    
+    return(data.frame(Y1=Y1, Y0=Y0))
+  }
+  
+}
+
+
+
+
+
