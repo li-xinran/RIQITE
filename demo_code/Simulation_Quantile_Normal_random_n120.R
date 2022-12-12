fec7307cdd676a5b4d0ed66658a8ab57d0352087
lmiratrix <luke@vzvz.org>
2022-12-12 10:57:58 -0500

    Tweaked simulation script to use new code.  TO CHECK: Is the stuff being saved in the right format for the post-simulation analysis?

--- demo_code/Simulation_Quantile_Normal_random_n120.R
+++ demo_code/Simulation_Quantile_Normal_random_n120.R
@@ -3,13 +3,16 @@
 # Run simulation in the paper using the RIQITE package.
 #
 
+# This tells us what simulation scenario to run (indexed by jobid)
 jobid <- as.numeric(Sys.getenv("SLURM_ARRAY_TASK_ID"))
-set.seed(jobid)
 setwd( "~/scratch/Simulation_Quantile_Normal_random_20221017" )
+
+set.seed(jobid * 13)
 
 # jobid = 28
 # setwd("~/Documents/Quantile_Simulation_2022Sep/Simulation_Quantile_Normal_random_20221017")
 
+library(tidyverse)
 library(RIQITE)
 library(utils)
 # library(mvtnorm)
@@ -30,52 +33,78 @@ omega = para.table$omega[jobid]
 rho = para.table$rho[jobid]
 
 
-# 1 + omega^2 + 2 * rho * omega
-
-normal_gen_PO <- function(n){
-  # Y0tau = rmvnorm( n, mean = c(0, tau0),
-  #         sigma = matrix(c(1, rho*omega, rho*omega, omega^2), nrow = 2) )
-  # Y0 = Y0tau[, 1]
-  # Y1 = Y0tau[, 1] + Y0tau[, 2]
-
-  Y0 = rnorm(n)
-  epsilon = rnorm(n)
-  tau = omega * ( rho * Y0 + sqrt(1-rho^2) * epsilon ) + tau0
-  Y1 = Y0 + tau
-
-  return(data.frame(Y1=Y1, Y0=Y0))
-}
 
 
-result = simu_power( gen = normal_gen_PO, n = 120, treat.prop = 0.5, iter.max = iter.max,
-                     Steph.s.vec = c(2, 4, 6, 10, 30, 60),
-                     alternative = "greater", alpha = sign.level,
-                     nperm = nperm, tol = 10^(-3), switch = TRUE )
+#### Step 1: Getting mean CIs on number of significant units ####
 
-perform = summary_power(result, measure = "CI_n(c)", alternative = "greater", cutoff = 0)
+alt <- explore_stephenson_s( s = c(2, 4, 6, 10, 30, 60),
+                             n = 120,
+                             Y0_distribution = rnorm,
+                             tx_function = "linear",
+                             ATE = tau0,
+                             tx_scale = omega * sqrt(1 - rho^2),
+                             rho = omega * rho,
+                             p_tx = 0.5,
+                             R = iter.max, iter_per_set = 10,
+                             alpha = sign.level,
+                             c = 0,
+                             quantile_n_CI = NA,
+                             targeted_power = FALSE,
+                             alternative = "greater",
+                             nperm = 1000,
+                             k.vec = NULL,
+                             parallel = FALSE,
+                             calc_ICC = FALSE,
+                             verbose = TRUE )
 
-pval.dim = rep(NA, iter.max)
 
-for(iter in 1:iter.max){
-  dat = result$dat.all[[iter]]
-  Z = result$Z.CRE[, iter]
-  Y = Z * dat$Y1 + (1-Z) * dat$Y0
+# The above gives stats for each quantile.  The stats on n is the last
+# column, the same will be listed across all quantiles for a given s
+# as the CI for n is for the entire analysis (all the quantiles
+# aggregated).
 
-  pval.dim[iter] = pval_bound(Z = Z, Y = Y, c = 0, method.list = list(name = "DIM"),
-                              alternative = "greater", Z.perm = result$Z.perm, impute = "control")
 
-  print(iter)
-}
+# Get the CI on n as a function of s by subsetting to the unique CIs
+# on n (rather than the individual stats on individual units)
+n_CIs <- alt %>% dplyr::select( s, n ) %>%
+    unique()
 
-power.dim = mean( pval.dim <= sign.level )
+n_CIs
+
+
+
+# Step 2: Looking at DiM for baseline, I think?
+
+alt_DIM <- calc_power( n = 120,
+                       Y0_distribution = rnorm,
+                       tx_function = "linear",
+                       ATE = tau0,
+                       tx_scale = omega * sqrt(1 - rho^2),
+                       rho = rho * omega,
+                       p_tx = 0.5,
+                       R = iter.max,
+                       percentile = 1,
+                       alpha = sign.level,
+                       targeted_power = TRUE,
+                       use_pval_bound = TRUE,
+                       c = 0,
+                       alternative = "greater",
+                       method.list = list(name = "DIM") )
+
+
+alt_DIM
+
+
+# Step 3: Save the results
+
 
 saveRDS( list(tau0 = tau0,
               omega = omega,
               rho = rho,
-              result = result,
-              perform = perform,
-              pval.dim = pval.dim,
-              power.dim = power.dim
+              result = alt,
+              perform = n_CIs,
+              pval.dim = alt_DIM,
+              power.dim = alt_DIM$power
               ),
          file = paste0("comp_n120_s_n0_", jobid, ".rds")
 )
